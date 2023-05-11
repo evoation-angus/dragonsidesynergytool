93a3142b524cc7b059a23a73ec69ef5213bf5873
Angus Horan <angus.horan@gmail.com>
2023-05-11 16:13:39 +1200

    revert to http

--- js/dragonside.js
+++ js/dragonside.js
@@ -13,7 +13,7 @@ function setTable() {
     table = $('#supportTable').DataTable({
         ajax: {
             type: "GET",
-            url: "https://dragonsidedata-env-2.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/currentadc",
+            url: "http://dragonsidedata-env-1.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/currentadc",
             dataSrc: "supports",
         },
         columns: [
@@ -64,7 +64,7 @@ function sendADCID(id) {
     var info = { ChampID: id };
     $.ajax({
         type: "POST",
-        url: "https://dragonsidedata-env-2.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/adcid",
+        url: "http://dragonsidedata-env-1.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/adcid",
         data: JSON.stringify(info),
         contentType: "application/json;",
         success: function (data) {
@@ -78,7 +78,7 @@ function sendADCID(id) {
 function getADC() {
     $.ajax({
         type: "GET",
-        url: "https://dragonsidedata-env-2.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/currentadc",
+        url: "http://dragonsidedata-env-1.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/currentadc",
         success: function (data) {
             var response = JSON.parse(data);
             var supports = response.supports;
@@ -113,7 +113,7 @@ function getADCnames() {
     document.getElementById("supportTable").style.visibility = "hidden"
     $.ajax({
         type: "GET",
-        url: "https://dragonsidedata-env-2.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/adcnames",
+        url: "http://dragonsidedata-env-1.eba-mkdkawv2.ap-southeast-2.elasticbeanstalk.com/adcnames",
         success: function (data) {
             //Parse request data
             console.log(data);
